481c01f1c05675e31f147f05bc90e31f7a36fee4
Sufiyan-11 <sufiyanshaikh111121998@gmail.com>
2023-05-02 17:46:33 +0530

    file updated

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.type_text', {
+    // Waits 1000ms after typing "First"
+    typeSpeed: 70,
+    loop: true,
+    backSpeed: 70,
+    smartBackspace: false,
+    strings: [' Programmer', 'Web Developer','Web Designer'],
+  });
