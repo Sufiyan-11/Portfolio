2efc352d9609349e83b8f734394d31b2632c786e
Sufiyan-11 <sufiyanshaikh111121998@gmail.com>
2023-05-04 15:16:22 +0530

    file updated

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ var typed = new Typed('.type_text', {
     smartBackspace: false,
     strings: ['Web Developer.','Programmer.','Web Designer.'],
   });
+
+  $(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+      // scroll body to 0px on click
+      $('#back-to-top').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 200);
+        return false;
+      });
+  });
